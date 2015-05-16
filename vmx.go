2a9941c40046e55991d097a976fc7651c542ef45
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-05-16 16:37:09 +0900

    List VMs in User document dir

--- vmx.go
+++ vmx.go
@@ -4,15 +4,52 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
-const VMRUN = "/Applications/VMware Fusion.app/Contents/Library/vmrun"
+const vmrun = "/Applications/VMware Fusion.app/Contents/Library/vmrun"
+
+var (
+	home = os.Getenv("HOME")
+	vmdir = fmt.Sprintf("%s/%s", home, "Documents/Virtual Machines.localized")
+)
+
+func vmname(vmpath string) string {
+	relpath := strings.TrimPrefix(vmpath, vmdir)
+	vmwarevm := path.Base(path.Dir(relpath))
+	return strings.TrimSuffix(vmwarevm, ".vmwarevm")
+}
+
+func list() (map[string]string) {
+	glob := fmt.Sprintf("%s/**/*.vmx", vmdir)
+	paths, err := filepath.Glob(glob)
+	if err != nil {
+		fmt.Printf("%s\n\n", err.Error())
+	}
+
+	vmx := make(map[string]string)
+	for _, path := range paths {
+		name := vmname(path)
+		vmx[name] = path
+	}
+
+	return vmx
+}
 
 func main() {
 	args := os.Args[1:]
-	out, err := exec.Command(VMRUN, args...).Output()
+	out, err := exec.Command(vmrun, args...).Output()
 	if err != nil {
 		fmt.Printf("%s\n\n", err.Error())
 	}
 	fmt.Printf("%s", out)
+
+	if len(args) > 0 && args[0] == "list" {
+		fmt.Printf("\nVMs in %s:\n", vmdir)
+		for name, path := range list() {
+			fmt.Printf("  %+20s (%s)\n", name, path)
+		}
+	}
 }
